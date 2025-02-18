96ec7390ebf2dab48dca90a517636290c165836e
JonathanRiche <j.riche64@gmail.com>
2025-02-18 11:16:06 -0500

    added path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import path from 'path';
 
 export default defineConfig({
 	plugins: [solidPlugin()],
@@ -9,4 +10,23 @@ export default defineConfig({
 	build: {
 		target: 'esnext',
 	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@blocks': path.resolve(__dirname, './src/blocks'),
+			'@commands': path.resolve(__dirname, './src/commands'),
+			'@css': path.resolve(__dirname, './src/css'),
+			'@design': path.resolve(__dirname, './src/design'),
+			'@ecommerce': path.resolve(__dirname, './src/ecommerce'),
+			'@events': path.resolve(__dirname, './src/events'),
+			'@multiplayer': path.resolve(__dirname, './src/multiplayer'),
+			'@panels': path.resolve(__dirname, './src/panels'),
+			'@plugins': path.resolve(__dirname, './src/plugins'),
+			'@server': path.resolve(__dirname, './src/server'),
+			'@toolbar': path.resolve(__dirname, './src/toolbar'),
+			'@traits': path.resolve(__dirname, './src/traits'),
+			'@types': path.resolve(__dirname, './src/types'),
+			'@utilities': path.resolve(__dirname, './src/utilities')
+		}
+	}
 });
